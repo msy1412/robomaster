26d02744573349e8df4d939e218bfa1f6a999557
PiggyGaGa <liana_gyd@163.com>
2017-11-04 04:10:25 -0400

    add CMakeList.txt file

---
+++ build/robomaster/CMakeFiles/masters.dir/DependInfo.cmake
@@ -0,0 +1,32 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/liana/document/VisualStudioCode/robomasterReWrite/robomaster/AngleSolver.cpp" "/home/liana/document/VisualStudioCode/robomasterReWrite/build/robomaster/CMakeFiles/masters.dir/AngleSolver.cpp.o"
+  "/home/liana/document/VisualStudioCode/robomasterReWrite/robomaster/AngleSolverFactory.cpp" "/home/liana/document/VisualStudioCode/robomasterReWrite/build/robomaster/CMakeFiles/masters.dir/AngleSolverFactory.cpp.o"
+  "/home/liana/document/VisualStudioCode/robomasterReWrite/robomaster/ArmorDetector.cpp" "/home/liana/document/VisualStudioCode/robomasterReWrite/build/robomaster/CMakeFiles/masters.dir/ArmorDetector.cpp.o"
+  "/home/liana/document/VisualStudioCode/robomasterReWrite/robomaster/ImageConsProd.cpp" "/home/liana/document/VisualStudioCode/robomasterReWrite/build/robomaster/CMakeFiles/masters.dir/ImageConsProd.cpp.o"
+  "/home/liana/document/VisualStudioCode/robomasterReWrite/robomaster/Predictor.cpp" "/home/liana/document/VisualStudioCode/robomasterReWrite/build/robomaster/CMakeFiles/masters.dir/Predictor.cpp.o"
+  "/home/liana/document/VisualStudioCode/robomasterReWrite/robomaster/RMVideoCapture.cpp" "/home/liana/document/VisualStudioCode/robomasterReWrite/build/robomaster/CMakeFiles/masters.dir/RMVideoCapture.cpp.o"
+  "/home/liana/document/VisualStudioCode/robomasterReWrite/robomaster/ResFilter.cpp" "/home/liana/document/VisualStudioCode/robomasterReWrite/build/robomaster/CMakeFiles/masters.dir/ResFilter.cpp.o"
+  "/home/liana/document/VisualStudioCode/robomasterReWrite/robomaster/RuneDetector.cpp" "/home/liana/document/VisualStudioCode/robomasterReWrite/build/robomaster/CMakeFiles/masters.dir/RuneDetector.cpp.o"
+  "/home/liana/document/VisualStudioCode/robomasterReWrite/robomaster/Serial.cpp" "/home/liana/document/VisualStudioCode/robomasterReWrite/build/robomaster/CMakeFiles/masters.dir/Serial.cpp.o"
+  "/home/liana/document/VisualStudioCode/robomasterReWrite/robomaster/Settings.cpp" "/home/liana/document/VisualStudioCode/robomasterReWrite/build/robomaster/CMakeFiles/masters.dir/Settings.cpp.o"
+  "/home/liana/document/VisualStudioCode/robomasterReWrite/robomaster/masters.cpp" "/home/liana/document/VisualStudioCode/robomasterReWrite/build/robomaster/CMakeFiles/masters.dir/masters.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/OpenCV/include"
+  "/usr/local/OpenCV/include/opencv"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/robomaster/CMakeFiles/masters.dir/cmake_clean.cmake
@@ -0,0 +1,20 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/masters.dir/RMVideoCapture.cpp.o"
+  "CMakeFiles/masters.dir/ArmorDetector.cpp.o"
+  "CMakeFiles/masters.dir/AngleSolver.cpp.o"
+  "CMakeFiles/masters.dir/AngleSolverFactory.cpp.o"
+  "CMakeFiles/masters.dir/RuneDetector.cpp.o"
+  "CMakeFiles/masters.dir/masters.cpp.o"
+  "CMakeFiles/masters.dir/Predictor.cpp.o"
+  "CMakeFiles/masters.dir/ImageConsProd.cpp.o"
+  "CMakeFiles/masters.dir/ResFilter.cpp.o"
+  "CMakeFiles/masters.dir/Settings.cpp.o"
+  "CMakeFiles/masters.dir/Serial.cpp.o"
+  "bin/masters.pdb"
+  "bin/masters"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/masters.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
